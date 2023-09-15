6aeec6fc9ced2730e497eb2d57e66b33ce08b4f3
Daniel Saavedra <daniel.saavedra.fon@gmail.com>
2023-09-15 12:37:41 -0500

    Commit temporal

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,13 +15,7 @@ java {
 repositories {
     mavenCentral()
 }
-configurations.all {
-    resolutionStrategy.eachDependency {
-        if (requested.name == "snakeyaml") {
-            useVersion("2.0")
-        }
-    }
-}
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
